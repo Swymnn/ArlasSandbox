e195528c9be672bf2fa907d2f4fc7e14edd37f19
VENTURE Ugo <ugo.venture@capgemini.com>
2025-03-25 15:34:09 +0100

    Init project with Arlas Tool Kit dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArlasToolKitModule } from 'arlas-wui-toolkit';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ArlasToolKitModule
   ],
   providers: [],
   bootstrap: [AppComponent]
